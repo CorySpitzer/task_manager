efb9e6369063ae3a2aed9baccdf60862ea073786
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 09:48:53 -0700

    Clear tasks before running specs and test that .all returns all the tasks

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,9 +2,20 @@ require('rspec')
 require('task')
 
 describe('Task') do
+  before do
+    Task.clear
+  end
+
   describe('#description') do
     it('returns a description') do
       expect(Task.new('wash a zebra').description).to(eq('wash a zebra'))
+    end
+  end
+
+  describe('.all') do
+    it('returns an array of task objects') do
+      task = Task.new('wash a zebra')
+      expect(Task.all).to(eq(['wash a zebra']))
     end
   end
 end
