b0d7d8be71ed77f5d46be64aa0ed24d6809b6b2b
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 11:56:03 -0700

    Test that the /clear path clears tasks from task manager

--- spec/task_integration_spec.rb
+++ spec/task_integration_spec.rb
@@ -10,3 +10,15 @@ describe('the tasks path', :type => :feature) do
     expect(page).to(have_content('Task: pet the zebra'))
   end
 end
+
+describe('the clear path', :type => :feature) do
+  it('clears all tasks from the Task Manager') do
+    visit('/')
+    fill_in('task-description', :with => 'pet the zebra')
+    click_button('add task')
+    click_link('Back')
+
+    click_button('clear all tasks')
+    expect(page).not_to(have_content('Task: pet the zebra'))
+  end
+end
