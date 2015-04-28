a124aeb3daa2d75f0ede10f5f5cd2b51bb35704d
Cory Spitzer & Garrett Olson <student@epicodus.com>
2015-04-28 11:31:53 -0700

    Change fill_in target to be accurate

--- spec/task_integration_spec.rb
+++ spec/task_integration_spec.rb
@@ -5,8 +5,8 @@ Capybara.app = Sinatra::Application
 describe('the tasks path', :type => :feature) do
   it('visit index and process user input') do
     visit('/')
-    fill_in('task', :with => 'pet the zebra')
-    click_button('add')
+    fill_in('task-description', :with => 'pet the zebra')
+    click_button('add task')
     expect(page).to(have_content('Task: pet the zebra'))
   end
 end
